c98468e9d269f3f45b474ed2cd4a4e2796796004
Anthony Li <38049831+doccstat@users.noreply.github.com>
2022-11-01 21:04:10 -0500

    initial package

---
+++ R/get_power.R
@@ -0,0 +1,15 @@
+#' Obtain the power analysis on a model.
+#'
+#' @param fixed_effects List of names of fixed effects.
+#' @param random_effects List of names of random effects.
+#' @param effect_size Effective sample size.
+#' @param effect_variance Effective sample variance.
+#'
+#' @return Power analysis results.
+#' @export
+get_power <- function(fixed_effects = NULL,
+                      random_effects = NULL,
+                      effect_size = NULL,
+                      effect_variance = NULL) {
+  return()
+}
