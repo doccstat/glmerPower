f66abb70f777171dfd3f97175287d9e00c0bf086
Anthony Li <38049831+doccstat@users.noreply.github.com>
2022-11-27 15:28:46 -0600

    get_model

--- R/get_model.R
+++ R/get_model.R
@@ -1,31 +1,62 @@
 #' Obtain a model from supplied parameters.
 #'
-#' @param fixed_effects List of names of fixed effects.
-#' @param random_effects List of names of random effects.
-#' @param effect_size Effective sample size.
-#' @param effect_sd Effective sample standard deviation.
-#' @param response Response variable
-#' @param family Family of model
-#' @param size Number of observations
+#' @param fixed_effects List of names of fixed effects including both continuous
+#'   and categorical variables.
+#' @param effect_size Effect size of the fixed effects.
+#' @param discrete_indicators List of boolean values indicating whether the
+#'   corresponding fixed effect is discrete or continuous.
+#' @param discrete_effects_levels List of levels of the discrete effects.
+#'   Corresponding elements are NULL for continuous effects and a vector of
+#'   levels for discrete effects.
+#' @param random_effect List of names of random effects.
+#' @param random_effects_cov Variance and covariances for random effects.
+#' @param residual_sd Residual standard deviation.
+#' @param response Response variable name.
+#' @param family Family of model.
 #'
 #' @return model with supplied parameters
 #' @export
 get_model <- function(fixed_effects = NULL,
-                      random_effects = NULL,
                       effect_size = NULL,
-                      effect_sd = NULL,
+                      discrete_indicators = NULL,
+                      discrete_effects_levels = NULL,
+                      random_effect = NULL,
+                      random_effects_cov = NULL,
+                      residual_sd = 1,
                       response = NULL,
-                      family = NULL,
-                      size = NULL) {
+                      family = NULL) {
 
-  fit <- lme4::lmer(response ~ fixed_effects + (1 | random_effects),
-                    data = data.frame(response = response,
-                                      fixed_effects = fixed_effects,
-                                      random_effects = random_effects),
-                    REML = FALSE,
-                    family = family)
-  fit@beta <- effect_size
-  fit@theta <- effect_sd
+  data <- list()
+  for (fixed_effect_index in seq_len(length(fixed_effects))) {
+    fixed_effect <- fixed_effects[fixed_effect_index]
+    if (discrete_indicators[fixed_effect_index]) {
+      data[[fixed_effect]] <- discrete_effects_levels[[fixed_effect_index]]
+    } else {
+      data[[fixed_effect]] <- seq(0, 1, length.out = 10)
+    }
+  }
+  data <- expand.grid(data)
+  fit <- if (is.null(family)) {
+    simr::makeLmer(
+      formula = as.formula(paste0(response, " ~ ",
+                                  paste(fixed_effects, collapse = " + "),
+                                  " + (1 | ", random_effect, ")")),
+      fixef = fixed_effects,
+      VarCorr = random_effects_cov,
+      sigma = residual_sd,
+      data = data
+    )
+  } else {
+    simr::makeGlmer(
+      formula = as.formula(paste0(response, " ~ ",
+                                  paste(fixed_effects, collapse = " + "),
+                                  " + (1 | ", random_effect, ")")),
+      family = family,
+      fixef = fixed_effects,
+      VarCorr = random_effects_cov,
+      data = data
+    )
+  }
 
   return(fit)
 }
