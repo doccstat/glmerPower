f34c8bdae9411d8948306c9cc3a7b019c9f871a4
carsonaj <carson.a.james@gmail.com>
2022-11-27 16:44:21 -0600

    update

--- R/get_model.R
+++ R/get_model.R
@@ -5,12 +5,12 @@
 #' @param effect_size Effect size of the fixed effects.
 #' @param discrete_indicators List of boolean values indicating whether the
 #'   corresponding fixed effect is discrete or continuous.
-#' @param discrete_effect_levels List of levels of the discrete effects.
+#' @param discrete_effects_levels List of levels of the discrete effects.
 #'   Corresponding elements are NULL for continuous effects and a vector of
 #'   levels for discrete effects.
 #' @param random_effect List of names of random effects.
 #' @param random_effect_levels Levels of the random effects.
-#' @param random_effects_cov Variance and covariances for random effects.
+#' @param random_effect_cov Variance and covariances for random effects.
 #' @param residual_sd Residual standard deviation.
 #' @param response Response variable name.
 #' @param family Family of model.
@@ -20,10 +20,10 @@
 get_model <- function(fixed_effects = NULL,
                       effect_size = NULL,
                       discrete_indicators = NULL,
-                      discrete_effect_levels = NULL,
+                      discrete_effects_levels = NULL,
                       random_effect = NULL,
                       random_effect_levels = NULL,
-                      random_effects_cov = NULL,
+                      random_effect_cov = NULL,
                       residual_sd = 1,
                       response = NULL,
                       family = NULL) {
@@ -32,20 +32,20 @@ get_model <- function(fixed_effects = NULL,
   for (fixed_effect_index in seq_len(length(fixed_effects))) {
     fixed_effect <- fixed_effects[fixed_effect_index]
     if (discrete_indicators[fixed_effect_index]) {
-      data[[fixed_effect]] <- discrete_effect_levels[[fixed_effect_index]]
+      data[[fixed_effect]] <- discrete_effects_levels[[fixed_effect_index]]
     } else {
       data[[fixed_effect]] <- seq(0, 1, length.out = 10)
     }
   }
-  data[[random_effect]] <- random_effect_levels
+  data[[random_effect]] <- random_effects_levels
   data <- expand.grid(data)
   fit <- if (is.null(family)) {
     simr::makeLmer(
       formula = stats::as.formula(paste0(response, " ~ ",
                                          paste(fixed_effects, collapse = " + "),
-                                         " + (1 | ", random_effect, ")")),
+                                         " + (1 | ", random_effects, ")")),
       fixef = effect_size,
-      VarCorr = random_effects_cov,
+      VarCorr = random_effect_cov,
       sigma = residual_sd,
       data = data
     )
--- tests/testthat/test_get_power.R
+++ tests/testthat/test_get_power.R
@@ -1,13 +1,13 @@
 use_r("foofy.R")
 
 # generate data:
-fixed_effects <- c("x")
-effect_size <- c(2, -0.1)
-discrete_indicators <- c(TRUE)
-discrete_effects_levels <- list(seq_len(10))
+fixed_effects <- c("x", "var2")
+effect_size <- c(2, -0.1, 2)
+discrete_indicators <- c(TRUE, FALSE)
+discrete_effects_levels <- list(seq_len(10), NULL)
 random_effect <- "g"
 random_effect_levels <- letters[1:3]
-random_effects_cov <- 0.5
+random_effect_cov <- 0.5
 residual_sd <- 1
 response <- "y"
 family <- NULL
@@ -18,12 +18,12 @@ fit <- get_model(fixed_effects = fixed_effects,
                  discrete_effects_levels = discrete_effects_levels,
                  random_effect = random_effect,
                  random_effect_levels = random_effect_levels,
-                 random_effects_cov = random_effects_cov,
+                 random_effect_cov = random_effect_cov,
                  residual_sd = residual_sd,
                  response = response,
                  family = family)
 
-
+fit
 
 
 
